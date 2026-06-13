922f923bdfa16cc82984b6bc11d0b00cb7d0572d
Reno <langmarbastien@MacBook-Pro-de-Langmar.local>
2026-06-13 19:15:15 +0200

    ajout des annotations: ojectifs, resultats attendus et resultats obtenus

--- requettes-test.sql
+++ requettes-test.sql
@@ -1,4 +1,7 @@
 /* Requête 1 : Ingrédients de la Raclaccia */
+-- objectif affichier les ingrédients de la Raclaccia
+-- résultat attendu : base tomate, raclette, cresson, ail, champignon, parmesan, poivre
+-- résultat obtenu: le résultat attendu
 
 SELECT i.nom_ingredient
 FROM ingredient i
@@ -10,12 +13,19 @@ WHERE f.nom_foccacia = 'Raclaccia';
 
 
 /* Requête 2 : Nombre total d'ingrédients */
+-- objectif: afficher le nombre total d'ingrédient
+-- résultat attendu: 25
+-- résultat obtenu: nombre_ingredient: 25
 
 SELECT COUNT(*) AS nombre_ingredients
 FROM ingredient;
 
 
 /* Requête 3 : Ingrédients non utilisés dans aucune foccacia */
+-- objectif: afficher les ingrédients non utilisés
+-- résultat attendu: salami, tomates-cerises
+-- résultat obtenu: nom_ingrédients: salami, tomates cerises
+
 
 SELECT i.nom_ingredient
 FROM ingredient i
@@ -25,6 +35,9 @@ WHERE c.id_ingredient IS NULL;
 
 
 /* Requête 4 : Liste des foccacias par ordre alphabétique */
+-- objectif: afficher les foccacias dans l'ordre alphabetique
+-- résultat attendu: americaine, emmentalaccia, gorgonzollaccia, hawaienne, mozaccia, paysanne, raclaccia, tradizione
+-- résultat obtenu: nom_foccacia1: americaine, emmentalaccia, gorgonzollaccia, hawaienne, mozaccia, paysanne, raclaccia, tradizione
 
 SELECT nom_foccacia
 FROM foccacia
@@ -32,12 +45,18 @@ ORDER BY nom_foccacia ASC;
 
 
 /* Requête 5 : Prix moyen des foccacias */
+--objectif: afficher le prix moyen des foccacias
+--resultat attendu: 10.3€
+-- resultat obtenu: 10.3€
 
 SELECT AVG(prix) AS prix_moyen
 FROM foccacia;
 
 
 /* Requête 6 : Nombre d'ingrédients par foccacia */
+-- objectif: afficher le nombre d'ingrédient par foccacia
+-- résultat attendu: americaine: 8, emmentalaccia:7, Gorgonzollaccia: 8, Hawaienne:9, Mozaccia:10, Paysanne:12, Raclaccia:7, Tradizione: 9
+-- résultat obtenu: resultat attendu
 
 SELECT f.nom_foccacia,
        COUNT(c.id_ingredient) AS nombre_ingredients
@@ -48,6 +67,9 @@ GROUP BY f.id_foccacia, f.nom_foccacia;
 
 
 /* Requête 7 : Foccacia contenant le plus d'ingrédients */
+-- objectif: afficher le foccacia avec le plus d'ingrédients
+-- résultat attendu: paysanne
+-- résultat obtenu: paysanne
 
 SELECT f.nom_foccacia,
        COUNT(c.id_ingredient) AS nombre_ingredients
@@ -60,6 +82,9 @@ LIMIT 1;
 
 
 /* Requête 8 : Foccacias contenant de l'ail */
+-- objectif: afficher les foccacia contenant de l'ail
+-- resultats attendus: Mozaccia, gorgonzollaccia, raclaccia, paysanne
+-- resultats obtenus: résultat attendu
 
 SELECT DISTINCT f.nom_foccacia
 FROM foccacia f
@@ -71,6 +96,9 @@ WHERE i.nom_ingredient = 'Ail';
 
 
 /* Requête 9 : Foccacias sans champignon */
+-- objectif: afficher les foccacias sans champignons
+-- resultats attendus: hawaienne, americaine
+-- resultats obtenus: resultats attendus
 
 SELECT nom_foccacia
 FROM foccacia
@@ -84,6 +112,9 @@ WHERE id_foccacia NOT IN (
 
 
 /* Requête 10 : Liste des boissons avec leur marque */
+-- objectif: afficher la liste des boissons avec leur marque
+-- resultats attendus: Coca-cola zéro,Coca-cola; Coca-cola original,Coca-cola; Capri-sun, Coca-cola; Fanta citron, Coca-cola; Fanta orange, coca-cola; Lipton peach, pepsico, Lipton zéro citron, pepsico; Pepsi, Pepsico; Pepsi Max Zéro, Pepsico; Monster energy ultra gold, Monster; Monster energy ultra blue, Monster; Eau de source, Cristaline
+-- resultats obtenus: resultats attendu
 
 SELECT b.nom_boisson,
        m.nom_marque
